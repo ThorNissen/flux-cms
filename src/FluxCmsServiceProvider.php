be7d5f8020c823b3d1d40cffd14883cb73f3bfc8
Thor Nissen <thorjnissen@gmail.com>
2024-11-06 19:57:33 +0100

    initial commit

---
+++ src/FluxCmsServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ThorNissen\FluxCms;
+
+use Illuminate\Support\ServiceProvider;
+
+class FluxCmsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
